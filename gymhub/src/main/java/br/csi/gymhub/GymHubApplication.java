806d74a49ec5d8e27ceed982f5b96a146bc27d5b
MrAbaddy <Vinicius_abaddy@hotmail.com>
2025-09-16 09:57:34 -0300

    Project Initializer

---
+++ gymhub/src/main/java/br/csi/gymhub/GymHubApplication.java
@@ -0,0 +1,13 @@
+package br.csi.gymhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GymHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GymHubApplication.class, args);
+	}
+
+}
---
+++ gymhub/src/test/java/br/csi/gymhub/GymHubApplicationTests.java
@@ -0,0 +1,13 @@
+package br.csi.gymhub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GymHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
