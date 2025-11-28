cdf2d364e03af11ea1a888d9ef4abe4d499cbd10
MrAbaddy <vinicius_abaddy@hotmail.com>
2025-11-28 04:32:08 -0300

    padronização de rota para usuario

--- src/main/java/br/csi/gymhub/controller/UsuarioController.java
+++ src/main/java/br/csi/gymhub/controller/UsuarioController.java
@@ -13,7 +13,7 @@ import java.net.URI;
 import java.util.List;
 
 @RestController
-@RequestMapping("/usuario")
+@RequestMapping("/gymhub/usuario")
 @AllArgsConstructor
 public class UsuarioController {
     private final UsuarioService service;
